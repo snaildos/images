fd085466f6d33d150f1a43a0071cc88c03c13b64
SnailDOS <46210117+snaildos@users.noreply.github.com>
2023-09-29 17:57:41 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,11 @@ echo ":/home/container$ ${MODIFIED_STARTUP}"
 toilet -f mono9 -F gay 'CreatOS'
 echo Initializing LavOS powered by CreatOS
 
+echo Uninstalling previous versions of Lavalink...
+rm Lavalink.jar -R
 echo Installing Lavalink v3.7.6...
 wget --max-redirect=0 https://cdn.snaildos.com/Lavalink.jar -q --show-progress
-wget --max-redirect=0 application.yml https://raw.githubusercontent.com/freyacodes/Lavalink/master/LavalinkServer/application.yml.example -o application.yml -q --show-progress
+echo Installed
 # Run the Server
 eval ${MODIFIED_STARTUP}
 echo LavaOS starting!
